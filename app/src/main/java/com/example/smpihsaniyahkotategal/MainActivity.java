b6c51f132dbc69c86f10ad1045d2a90517e16ba6
amelia705 <ameliafaradita12@gmail.com>
2022-03-01 12:43:30 +0700

    commit

--- app/src/main/java/com/example/smpihsaniyahkotategal/MainActivity.java
+++ app/src/main/java/com/example/smpihsaniyahkotategal/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.smpihsaniyahkotategal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewFlipper;
+
 
 public class MainActivity extends AppCompatActivity {
+    ViewFlipper viewFlipper;
+    Animation fadein, fadeout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+
+        fadein = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        fadeout = AnimationUtils.loadAnimation(this,R.anim.fade_out);
+
+        viewFlipper.setInAnimation(fadein);
+        viewFlipper.setOutAnimation(fadeout);
+
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.startFlipping();
+
+    }
+    public void profil(View view) {
+        Intent intent = new Intent(com.example.smpihsaniyahkotategal.MainActivity.this, ProfilActivity.class);
+        startActivity(intent);
+    }
+
+    public void guru(View view) {
+        Intent intent = new Intent(com.example.smpihsaniyahkotategal.MainActivity.this, GuruActivity.class);
+        startActivity(intent);
+    }
+
+    public void eskul(View view) {
+        Intent intent = new Intent(com.example.smpihsaniyahkotategal.MainActivity.this, EkskulActivity.class);
+        startActivity(intent);
+    }
+
+    public void fasilitas(View view) {
+        Intent intent = new Intent(com.example.smpihsaniyahkotategal.MainActivity.this, FasilitasActivity.class);
+        startActivity(intent);
+    }
+
+    public void perpus(View view) {
+        Intent intent = new Intent(com.example.smpihsaniyahkotategal.MainActivity.this, PerpusActivity.class);
+        startActivity(intent);
+    }
+
+    public void galeri(View view) {
+        Intent intent = new Intent(com.example.smpihsaniyahkotategal.MainActivity.this, GaleriActivity.class);
+        startActivity(intent);
     }
 }
+
