8c51dcab31abbbbb9ed0784343b0a1d0a759859a
andikarifky <andikarifky20@gmail.com>
2022-03-02 12:31:22 +0700

    commit

--- app/src/main/java/com/example/smpihsaniyahkotategal/FasilitasActivity.java
+++ app/src/main/java/com/example/smpihsaniyahkotategal/FasilitasActivity.java
@@ -2,7 +2,9 @@ package com.example.smpihsaniyahkotategal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FasilitasActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class FasilitasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fasilitas);
+    }
+
+    public void Kelas(View view) {
+        Intent intent = new Intent(FasilitasActivity.this, KelasActivity.class);
+        startActivity(intent);
     }
 }
