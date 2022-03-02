2a04a86a0ffd439d0d64ce77512dfdf39bf7a50f
amelia705 <ameliafaradita12@gmail.com>
2022-03-02 12:03:30 +0700

    commit

---
+++ app/src/main/java/com/example/smpihsaniyahkotategal/AlbumHutPgriActivity.java
@@ -0,0 +1,14 @@
+package com.example.smpihsaniyahkotategal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlbumHutPgriActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_hut_pgri);
+    }
+}
