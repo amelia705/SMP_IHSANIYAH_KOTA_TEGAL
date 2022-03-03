88d9386932a91e565181bc233659c24db79c597c
Dwiky16 <dwikydarmawan2004@gmail.com>
2022-03-03 15:25:10 +0700

    commit

--- app/src/main/java/com/example/smpihsaniyahkotategal/EkskulActivity.java
+++ app/src/main/java/com/example/smpihsaniyahkotategal/EkskulActivity.java
@@ -1,14 +1,44 @@
 package com.example.smpihsaniyahkotategal;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 public class EkskulActivity extends AppCompatActivity {
+    SliderView sliderView;
+    int[] images = {R.drawable.headerihs,
+            R.drawable.headergaleri1,
+            R.drawable.headerihs2,
+            R.drawable.poster1,
+            R.drawable.poster2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ekskul);
+
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+    }
+
+    public void musik(View view) {
+    }
+
+    public void lari(View view) {
+    }
+
+    public void bulutangkis(View view) {
     }
 }
